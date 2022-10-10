e070190cc12e7f5f6b5e74e007642d17062a55d9
liyasandeep <liyasandeep1@gmail.com>
2022-10-10 16:36:29 +0100

    task 4 get/api/users not complete

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const getTopics = require("./controllers/topicsController");
+const getUsers = require("./controllers/usersController.js");
 const app = express();
 
 app.get("/api/topics", getTopics);
 
+app.get("/api/users", getUsers);
 app.all("*", (req, res) => {
   res.status(404).send({ message: "Invalid Route!" });
 });
---
+++ controllers/usersController.js
@@ -0,0 +1,13 @@
+const selectUsers = require("../models/usersModel");
+
+const getUsers = (req, res, next) => {
+  selectUsers()
+    .then((users) => {
+      res.status(200).send({ users });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+module.exports = getUsers;
