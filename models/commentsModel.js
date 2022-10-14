31aff87353bad910b212a38a386d661d6ffe21d6
liyasandeep <liyasandeep1@gmail.com>
2022-10-14 12:07:16 +0100

    Remove console log

--- models/commentsModel.js
+++ models/commentsModel.js
@@ -23,7 +23,6 @@ const insertCommentByArticleId = (article_id, username, body) => {
 const removeCommentByCommentId = (comment_id) => {
   let queryStr = ` DELETE FROM comments WHERE comment_id = $1;`;
   return db.query(queryStr, [comment_id]).then(({ rowCount }) => {
-    console.log(rowCount, comment_id);
     if (rowCount === 0) {
       return Promise.reject({ status: 404, message: "Id not found" });
     }
